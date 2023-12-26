9ec11bc5d9d5280eeb6aaa5e53ffa6f04c81c4fd
zoomkolol <56260430+zoomkolol@users.noreply.github.com>
2023-12-26 13:21:42 +0300

    First changes

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './styles/index.css';
+
+const input = document.querySelector("#promocode");
+
+function filterPromoCodes() {
+  const filter = input.value.toUpperCase();
+  const cards = document.querySelectorAll(".card");
+
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    const promocode = card.querySelector(".card__promocode");
+    const cardText = promocode.textContent;
+
+    if (cardText.toUpperCase().indexOf(filter) > -1) {
+      card.style.display = "flex";
+    } else {
+      card.style.display = "none";
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  input.addEventListener('input', filterPromoCodes);
+  filterPromoCodes();
+});
